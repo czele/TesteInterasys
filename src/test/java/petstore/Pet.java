4ff683757ab4010b136cfdf07a5a01a0dbabe88d
czele <camila_zele@hotmail.com>
2022-04-14 16:42:45 -0300

    given feito

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,13 +1,24 @@
 package petstore;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
 
 public class Pet {
     String uri : "virtserver.swaggerhub.com/camilazele/pet/1.0.0";
 
-    public String lerJson(String caminhoJson){
+    public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+
+        given()
+                .contentType("apllication/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
     }
 }
