17e3733f95a21546a89e0ae37fec3201156af2b2
czele <camila_zele@hotmail.com>
2022-04-18 13:06:03 -0300

    resolvi problema do swagger

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,8 +1,6 @@
 package petstore;
 
-import io.restassured.RestAssured;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,15 +8,15 @@ import java.nio.file.Paths;
 import static io.restassured.RestAssured.given;
 
 public class Pet {
-    String uri : "virtserver.swaggerhub.com/camilazele/pet/1.0.0";
+    String uri = "https://petstore.swagger.io";
 
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
 
-        given()
-                .contentType("apllication/json")
-                .log().all()
-                .body(jsonBody)
-        .when()
+        @Test
+        public void IncluirPet(){
+            String jsonBody = lerJson("db/pet1.json");
+        }
+
     }
 }
