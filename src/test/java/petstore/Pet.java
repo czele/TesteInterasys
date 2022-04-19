901952e20c99167ad4dade602db74346c87358f5
czele <camila_zele@hotmail.com>
2022-04-19 16:23:23 -0300

    arrumei todos os erros

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,17 +1,20 @@
 package petstore;
 
 import org.testng.annotations.Test;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class Pet {
     String uri = "https://petstore.swagger.io";
 
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
 
         @Test
         public void IncluirPet(){
